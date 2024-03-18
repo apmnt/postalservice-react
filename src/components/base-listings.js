c2c0857a5ce9e59b09a8d581e64c383cd37b7776
apmnt <aapo.montin@student.oulu.fi>
2024-03-18 15:36:58 +0200

    Add base component for listing components

---
+++ src/components/base-listings.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+import { Listing } from "./listing";
+import LoadMoreButton from "./buttons/load-more-button";
+
+export const BaseListings = ({
+  site,
+  keyword,
+  size,
+  title,
+  initialVisibleCount = 40,
+  loadMoreStep = 40,
+}) => {
+  const [listings, setListings] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(initialVisibleCount);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [reachedEnd, setReachedEnd] = useState(false);
+
+  const fetchMoreProducts = async (page) => {
+    console.log("fetchMoreProducts", page, site, keyword, size, loadMoreStep);
+    if (reachedEnd) {
+      return;
+    }
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `https://postalservice-flask-api.vercel.app/?site=${site}&keyword=${keyword}&size=${size}&page=${page}`
+      );
+      setListings((prevListings) => [...prevListings, ...response.data]);
+      if (response.data.length < loadMoreStep) {
+        setReachedEnd(true);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching more products:", error);
+      setLoading(false);
+    }
+  };
+
+  const loadMore = useCallback(() => {
+    fetchMoreProducts(page);
+    setPage(page + 1);
+    setVisibleCount(visibleCount + loadMoreStep);
+    console.log(
+      "page",
+      page,
+      "listings",
+      listings.length,
+      "visibleCount",
+      visibleCount
+    );
+  }, [visibleCount, listings.length, page, fetchMoreProducts, loadMoreStep]);
+
+  useEffect(() => {
+    const fetchInitialProducts = async () => {
+      let currentPage = 1;
+      await fetchMoreProducts(currentPage);
+      currentPage++;
+      await fetchMoreProducts(currentPage);
+      setPage(page + 2);
+    };
+    fetchInitialProducts();
+  }, []);
+
+  return (
+    <div className="listings">
+      <h2 className="listings__title">{title}</h2>
+      <div className="listings__grid">
+        {listings.slice(0, visibleCount).map((listing, index) => (
+          <Listing
+            key={index}
+            title={listing.title}
+            price={Number(listing.price).toLocaleString("en-US", {
+              style: "currency",
+              currency: "JPY",
+            })}
+            size={listing.size}
+            pictureUrl={listing.img}
+            url={listing.url}
+          />
+        ))}
+      </div>
+      {loading && <div className="loader"> Loading new listings ...</div>}
+      {!loading && !reachedEnd && (
+        <LoadMoreButton
+          className="listings__load-more"
+          visibleCount={visibleCount}
+          listingsLength={listings.length}
+          loadMore={loadMore}
+        />
+      )}
+      <hr className="listings__line" />
+    </div>
+  );
+};
--- src/components/fril-listings.js
+++ src/components/fril-listings.js
@@ -1,51 +1,15 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Listing } from "./listing";
+import React from "react";
+import { BaseListings } from "./base-listings";
 
 export const FrilListings = () => {
-  const [listings, setListings] = useState([]);
-  const [visibleCount, setVisibleCount] = useState(12);
-
-  useEffect(() => {
-    const fetchListings = async () => {
-      try {
-        const response = await axios.get(
-          "https://postalservice-flask-api.vercel.app/?site=fril&keyword=yohji&size=M"
-        );
-        setListings(response.data);
-      } catch (error) {
-        console.error("Failed to fetch listings:", error);
-      }
-    };
-
-    fetchListings();
-  }, []);
-
-  const loadMore = () => {
-    setVisibleCount(visibleCount + 12);
-  };
-
   return (
-    <div className="listings">
-      <h2 className="listings__title">Fril Listings</h2>
-      <div className="listings__grid">
-        {listings.slice(0, visibleCount).map((listing, index) => (
-          <Listing
-            key={index}
-            title={listing.title}
-            price={Number(listing.price).toLocaleString("en-US", {
-              style: "currency",
-              currency: "JPY",
-            })}
-            size={listing.size}
-            pictureUrl={listing.img}
-            url={listing.url}
-          />
-        ))}
-      </div>
-      {visibleCount < listings.length && (
-        <button onClick={loadMore}>Load more</button>
-      )}
-    </div>
+    <BaseListings
+      site="fril"
+      keyword="junya"
+      size="XL"
+      title="Fril Listings"
+      initialVisibleCount={36}
+      loadMoreStep={36}
+    />
   );
 };
