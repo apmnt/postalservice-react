c1220b03d2ea772e67d7ccc23a6622134fe59da3
apmnt <aapo.montin@student.oulu.fi>
2024-03-18 20:01:27 +0200

    Update listing title to be a clickable link

--- src/components/listing.js
+++ src/components/listing.js
@@ -4,7 +4,9 @@ export const Listing = ({ title, price, size, pictureUrl, url }) => (
   <div className="listing">
     <img className="listing__picture" src={pictureUrl} alt={title} />
     <div className="listing__text">
-      <p className="listing__title">{title}</p>
+      <a href={url} className="listing__title">
+        {title}
+      </a>
       <p className="listing__size">{size}</p>
       <p className="listing__price">{price}</p>
     </div>
