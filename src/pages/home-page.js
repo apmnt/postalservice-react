5144f014ad69ad1bd0ed34256fe02943f1334090
apmnt <aapo.montin@student.oulu.fi>
2024-03-18 19:41:48 +0200

    Refactor home page to include search functionality and update listings components

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,12 +1,34 @@
-import React from "react";
-// import { Auth0Features } from "../components/auth0-features";
+import React, { useState } from "react";
 import { PageLayout } from "../components/page-layout";
-import { MercariListings } from "../components/mercari-listings";
-import { FrilListings } from "../components/fril-listings";
+import SearchBar from "../components/search-bar";
+import { BaseListings } from "../components/base-listings";
 
-export const HomePage = () => (
-  <PageLayout>
-    <MercariListings />
-    <FrilListings />
-  </PageLayout>
-);
+export const HomePage = () => {
+  const [searchTerm, setSearchTerm] = useState("junya");
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
+  return (
+    <PageLayout>
+      <SearchBar onSearch={handleSearch} />
+      <BaseListings
+        key={`mercari-${searchTerm}`}
+        site="mercari"
+        keyword={searchTerm}
+        size="XL"
+        title="Mercari Listings"
+      />
+      <BaseListings
+        key={`fril-${searchTerm}`}
+        site="fril"
+        keyword={searchTerm}
+        size="XL"
+        title="Fril Listings"
+        initialVisibleCount={36}
+        loadMoreStep={36}
+      />
+    </PageLayout>
+  );
+};
