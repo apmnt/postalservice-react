8252e7769e81e695b2f6b089e0f3729362ba15cd
apmnt <aapo.montin@student.oulu.fi>
2024-03-18 15:37:42 +0200

    Refactor load more button styling

--- src/components/buttons/load-more-button.js
+++ src/components/buttons/load-more-button.js
@@ -1,28 +1,23 @@
-// LoadMoreButton.js
 import React from "react";
 
 const LoadMoreButton = ({ visibleCount, listingsLength, loadMore }) => {
-  if (visibleCount < listingsLength) {
-    return (
-      <button
-        onClick={loadMore}
-        style={{
-          marginTop: "2rem",
-          marginLeft: "0",
-          paddingLeft: "0",
-          outline: "none",
-          fontSize: "2.2rem",
-          fontStyle: "italic",
-          backgroundColor: "transparent",
-          border: "none",
-        }}
-      >
-        Load more
-      </button>
-    );
-  }
-
-  return null;
+  return (
+    <button
+      onClick={loadMore}
+      style={{
+        display: "block",
+        margin: "2rem auto 0 auto",
+        paddingLeft: "0",
+        outline: "none",
+        fontSize: "2.6rem",
+        fontStyle: "italic",
+        backgroundColor: "transparent",
+        border: "none",
+      }}
+    >
+      Show more
+    </button>
+  );
 };
 
 export default LoadMoreButton;
