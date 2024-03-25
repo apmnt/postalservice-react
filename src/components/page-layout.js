2163b86557e4b36129e3e0e2cc7a5b77585aed56
apmnt <aapo.montin@student.oulu.fi>
2024-03-25 17:40:00 +0200

    Remove states from layout component

--- src/components/page-layout.js
+++ src/components/page-layout.js
@@ -1,26 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavBar } from "./navigation/desktop/nav-bar";
 import { MobileNavBar } from "./navigation/mobile/mobile-nav-bar";
 import { PageFooter } from "./page-footer";
-import Filters from "./filters";
 
-export const PageLayout = ({ children, onSearch }) => {
-  const [isSearchActive, setIsSearchActive] = useState(false);
-
-  const handleSearchClick = (keyword) => {
-    onSearch(keyword);
-    setIsSearchActive(!isSearchActive);
-  };
-
+export const PageLayout = ({ children }) => {
   return (
     <div className="page-layout">
       <NavBar />
-      <MobileNavBar
-        onSearch={handleSearchClick}
-        isSearchActive={isSearchActive}
-        setIsSearchActive={setIsSearchActive}
-      />{" "}
-      {isSearchActive && <Filters />}
+      <MobileNavBar />
       <div className="page-layout__content">{children}</div>
       <PageFooter />
     </div>
