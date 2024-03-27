4d81bfd9f2938adf2bbd40609950871111e7b76c
apmnt <aapo.montin@student.oulu.fi>
2024-03-27 18:20:59 +0200

    Add scroll effect to mobile navigation bar and adjust font size in listings.css

--- src/components/navigation/mobile/mobile-nav-bar-brand.js
+++ src/components/navigation/mobile/mobile-nav-bar-brand.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 export const MobileNavBarBrand = ({ handleClick }) => {
+  const titleRef = useRef();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        // replace 100 with the amount of scrolling you want
+        titleRef.current.classList.add("small");
+      } else {
+        titleRef.current.classList.remove("small");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div onClick={handleClick} className="mobile-nav-bar__brand">
       <NavLink to="/">
-        <h1 className="nav-bar__title-mobile">Postal Service</h1>
+        <h1 ref={titleRef} className="nav-bar__title-mobile">
+          Postal Service
+        </h1>
         {/* <img
           className="mobile-nav-bar__logo"
           src="https://cdn.auth0.com/blog/hub/code-samples/hello-world/auth0-logo.svg"
